7087111874bd7677ec8d8360fa4c31748672e152
Max <m412tinez@gmail.com>
2021-07-22 13:30:31 -0500

    setting up express to handle parsing

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,20 +1,25 @@
 const express= require('express');
 const path= require('path');
-const fs= require('fs');
-
 const PORT= 3000;
+// processs.env.PORT || 3001;
+
 const app= express();
 
+require('./routes/apiRoutes')(app)
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+
 app.listen(PORT, () => {
-    console.log(`listening on: ${PORT}`);
+    console.log(`Server listening on: http://localhost:${PORT}`);
 });
