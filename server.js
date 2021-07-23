ba6da903c2ff33a68f5288cf3333e0ab2b2d40d8
Max <m412tinez@gmail.com>
2021-07-23 00:27:55 -0500

    moved routes to server.js file and completed get and post request

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express= require('express');
 const path= require('path');
+const fs= require('fs')
 const PORT= 3000;
 // processs.env.PORT || 3001;
 
 const app= express();
-
-require('./routes/apiRoutes')(app)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,6 +18,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/api/notes', (req, res) =>
+        res.sendFile(path.join(__dirname, './db/db.json'))
+    );
+    app.get('/api/notes', (req, res) => res.JSON(data));
+    
+
+    app.post('/api/notes', (req, res) =>  {
+        const noteDB= req.body;
+        const notePar= JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+        
+        notePar.push(noteDB);
+        fs.writeFile('./db/db.json', JSON.stringify(notePar), (err) => {
+            if (err) throw err;
+        });
+        res.json(notePar);
+    });
 
 app.listen(PORT, () => {
     console.log(`Server listening on: http://localhost:${PORT}`);
